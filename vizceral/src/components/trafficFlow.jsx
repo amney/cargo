53c0b683b4b4ce732d7350f00828cbb9e9b12327
Tim Garner <timgarner0@gmail.com>
2019-07-31 21:12:03 -0700

    icons and title

--- vizceral/src/components/trafficFlow.jsx
+++ vizceral/src/components/trafficFlow.jsx
@@ -190,7 +190,7 @@ class TrafficFlow extends React.Component {
         this.state.currentView[1] !== nextState.currentView[1] ||
         this.state.highlightedObject !== nextState.highlightedObject) {
       const titleArray = (nextState.currentView || []).slice(0);
-      titleArray.unshift('Vizceral');
+      titleArray.unshift('Bottle Application Traffic Generator');
       document.title = titleArray.join(' / ');
 
       if (this.poppedState) {
